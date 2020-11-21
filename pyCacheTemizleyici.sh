10725be740f73d31164c4bc66ea30ef306d3c671
keyiflerolsun <keyiflerolsun@gmail.com>
2020-11-21 22:36:57 +0300

    PYPI Taslağı güncellendi..

--- pyCacheTemizleyici.sh
+++ pyCacheTemizleyici.sh
@@ -1,6 +1,3 @@
 # Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
 
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
-rm -rf build/
-rm -rf dist/
-rm -rf -- *.egg-info/
--- pypiYukleyici.sh
+++ pypiYukleyici.sh
@@ -1,4 +1,8 @@
 # Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
 
-python setup.py sdist bdist_wheel
+python setup.py bdist_wheel sdist
+twine check dist/*
 python -m twine upload dist/*
+rm -rf build/
+rm -rf dist/
+rm -rf -- *.egg-info/
