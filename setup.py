8592444180cc11e1b27b1775e4e50d948a61bca3
keyiflerolsun <keyiflerolsun@gmail.com>
2022-10-17 12:17:49 +0300

    🕊 v0.7.1 - SonDakika ve Sahibinden Onarıldı..
    🕊

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages     = ["KekikSpatula"],
 
     name         = "KekikSpatula",
-    version      = "0.7.3",
+    version      = "0.7.4",
     url          = "https://github.com/keyiflerolsun/KekikSpatula",
     description  = "Siz uğraşmayın diye biz uğraştık.. ~ dızz 🐍",
     keywords     = ["KekikSpatula", "KekikAkademi", "keyiflerolsun"],
